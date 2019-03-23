680138662fb5d34940b1cf768d8d7a60a25345e8
SahiltheGreat <smbstar1@gmail.com>
2019-03-23 23:56:04 +0530

    update

---
+++ Game.py
@@ -0,0 +1,95 @@
+import turtle
+import math
+import random
+import pygame
+import time
+
+#FOR  GENERATING SOUND ON HITTING TARGET
+pygame.init()
+pygame.mixer.music.load("point.wav")
+
+#INITIALISING PLAYAREA
+playarea= turtle.Screen()
+playarea.bgcolor("black")
+
+#SETTING BORDERS OF PLAYFIELD
+borderpen = turtle.Turtle()
+borderpen.penup()
+borderpen.color("white")
+borderpen.setposition(-300,-300)
+borderpen.pendown()
+borderpen.pensize(3)
+
+for side in range(4):
+    borderpen.forward(600)
+    borderpen.left(90)
+borderpen.hideturtle()
+
+
+#INITIALISING THE PLAYER
+player = turtle.Turtle()
+player.color("green")
+player.shape("triangle")
+player.penup()
+player.speed(0)
+
+
+#INITIALISING THE TARGET
+goal =turtle.Turtle()
+goal.color("red")
+goal.shape("circle")
+goal.penup()
+goal.speed(0)
+goal.setposition(random.randint(-300,300),random.randint(-300,300))
+
+#SETTING PLAYER SPEED
+speed = 1
+
+
+#DEFINING PLAYER FUNCTIONS
+def turnleft():
+    player.left(30)
+
+def turnright():
+    player.right(30)
+
+def increasespeed():
+    global speed
+    speed +=1
+
+def decreasespeed():
+    global speed
+    speed -=1
+
+def isCollision(p1,p2):
+    d = math.sqrt(math.pow(p1.xcor()-p2.xcor(),2) + math.pow(p1.ycor()-p2.ycor(),2))
+    if d < 20:
+        return True
+    else:
+        return False
+
+#FOR USER INPUT
+turtle.listen()
+turtle.onkey(turnleft,"Left")
+turtle.onkey(turnright,"Right")
+turtle.onkey(increasespeed,"Up")
+turtle.onkey(decreasespeed,"Down")
+
+#GAMEPLAY
+while True:
+    player.forward(speed)
+
+    #CHECKING BORDER COLLISION
+    if player.xcor()>300 or player.xcor()<-300:
+        player.right(180)
+    
+    if player.ycor()>300 or player.ycor()<-300:
+        player.right(180)
+
+    #CHECKING PLAYER-TARGET COLLISION
+    if isCollision(player,goal):
+        pygame.mixer.music.play()
+        time.sleep(0)
+        goal.setposition(random.randint(-300,300),random.randint(-300,300))
+
+
