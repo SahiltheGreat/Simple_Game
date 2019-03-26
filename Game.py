ea88eb9e330c3acd5f77978728537f52df3b98ef
SahiltheGreat <smbstar1@gmail.com>
2019-03-27 02:07:10 +0530

    added Bump sound

--- Game.py
+++ Game.py
@@ -8,7 +8,7 @@ import time
 pygame.init()
 s1=pygame.mixer.Sound("point.wav")
 s2=pygame.mixer.Sound("gameover.wav")
-
+s3=pygame.mixer.Sound("bump.wav")
 
 #INITIALISING PLAYAREA
 playarea= turtle.Screen()
@@ -120,7 +120,7 @@ class Timer(turtle.Turtle):
             turtle.bye()
 
 
-timer = Timer(0,310,"red",30)
+timer = Timer(0,310,"red",120)
 timer.start()
 
 
@@ -139,9 +139,13 @@ while True:
     #CHECKING BORDER COLLISION
     if player.xcor()>300 or player.xcor()<-300:
         player.right(180)
+        s3.play()
+        time.sleep(0)
     
     if player.ycor()>300 or player.ycor()<-300:
         player.right(180)
+        s3.play()
+        time.sleep(0)
 
     #CHECKING PLAYER-TARGET COLLISION
     if isCollision(player,goal):
